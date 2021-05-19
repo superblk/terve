23bac280e2a905627c346d28f095eb1923317d29
jpalomaki <jukka@jpalomaki.fi>
2021-05-19 22:52:26 +0300

    Save

--- README.md
+++ README.md
@@ -2,24 +2,31 @@
 
 Unified terraform and terragrunt version manager.
 
+NOTE: this is an early development version so no releases yet. :sob:
+
 ## Setup
 
-1. Download terve for your operating system
+1. Build terve for your operating system
 1. Install terve in `PATH`, e.g. in `/usr/local/bin`
 1. Add directory `~/.terve/bin` to `PATH` (using e.g. `.bashrc`)
 
 ## Usage
 
-NOTE: `<binary>` is either `tf` (`terraform`) or `tg` (`terragrunt`)
+NOTE: `<binary>` is one of:
+
+- `tf` (or: `terraform`)
+- `tg` (or: `terragrunt`)
 
 ### List
 
-Syntax: `terve l[ist] <binary> [spec]`
+Syntax: `terve l[ist] <binary> [spec]` where spec is `r[emote]`
 
 - `terve l tf` lists installed terraform versions
 - `terve l tg` lists installed terragrunt versions
-- `terve l tf remote` lists available terraform versions
-- `terve l tg remote | grep 0.29.` lists available terragrunt 0.29.x versions
+- `terve l tf r` lists available (remote) terraform versions
+- `terve l tg r | grep 0.29.` lists available terragrunt 0.29.x versions
+
+NOTE: list remote for terragrunt uses GitHub API which is rate-limited!
 
 ### Install
 
@@ -46,11 +53,16 @@ Syntax: `terve r[emove] <binary> <semver>`
 - `terve r tf 0.12.31` removes terraform version 0.12.31
 - `terve l tf | grep 0.11. | xargs -n1 -P4 terve r tf` removes all installed terraform 0.11.x versions
 
-NOTE: remove does not fail if given version is not installed
+NOTE: does not fail if version is not installed
+
+## Development
+
+You need rustup and cargo. See <https://rustup.rs/>
+
+To build the binary, run `cargo build --release`. Binary is then found in `target/release/terve`
 
 ## TODOs
 
-- GH workflow build
-- support macos
 - tests
+- GH workflow build (matrix -> linux/darwin)
 - implement GPG verify (terraform)
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use argh::FromArgs;
 use home::home_dir;
 use regex::Regex;
-use reqwest::Client;
+use reqwest::blocking::Client;
 use semver::Version;
 use serde::Deserialize;
 use sha2::{Digest, Sha256};
-use std::{error::Error, io::{Cursor, Read}, process, time::Duration};
+use std::{
+    error::Error,
+    io::{Seek, SeekFrom},
+    process,
+    time::Duration,
+};
 use std::{fmt::Display, path::PathBuf, str::FromStr};
 use std::{
     fs::{create_dir_all, read_dir, read_link, remove_file, File},
@@ -16,13 +21,13 @@ use std::{
     io::copy,
 };
 use zip::ZipArchive;
+use std::env::consts::OS;
 
-#[tokio::main]
-async fn main() {
-    process::exit(match run().await {
-        Ok(msg) => {
-            if !msg.is_empty() {
-                println!("{}", msg);
+fn main() {
+    process::exit(match run() {
+        Ok(s) => {
+            if !s.is_empty() {
+                println!("{}", s);
             }
             0
         }
@@ -33,30 +38,41 @@ async fn main() {
     });
 }
 
-async fn run() -> Result<String, Box<dyn Error>> {
+type StringOrError = Result<String, Box<dyn Error>>;
+
+fn run() -> StringOrError {
     let args: Args = argh::from_env();
     match home_dir() {
-        Some(h) => {
-            let dot_dir_path = create_dot_dir(h)?;
+        Some(home) => {
+            let dot_dir = DotDir::init(home)?;
+            let os = match OS {
+                "linux" => "linux",
+                "macos" => "darwin",
+                os => panic!("Unsupported OS: {}", os),
+            };
             match (args.action, args.binary, args.version_spec) {
-                (Action::LIST, binary, None) => list_installed_versions(binary, dot_dir_path),
-                (Action::LIST, Binary::TERRAFORM, Some(v)) if v == "remote" => {
-                    list_available_terraform_versions().await
+                (Action::LIST, binary, None) => list_installed_versions(binary, dot_dir),
+                (Action::LIST, Binary::TERRAFORM, Some(v)) if v == "r" || v == "remote" => {
+                    list_available_terraform_versions()
                 }
-                (Action::LIST, Binary::TERRAGRUNT, Some(v)) if v == "remote" => {
-                    list_available_terragrunt_versions().await
+                (Action::LIST, Binary::TERRAGRUNT, Some(v)) if v == "r" || v == "remote" => {
+                    list_available_terragrunt_versions()
                 }
-                (Action::INSTALL, Binary::TERRAFORM, Some(version)) if Version::parse(&version).is_ok() => {
-                    install_terraform_version(version, dot_dir_path).await
+                (Action::INSTALL, Binary::TERRAFORM, Some(version))
+                    if Version::parse(&version).is_ok() =>
+                {
+                    install_terraform_version(version, dot_dir, os)
                 }
-                (Action::INSTALL, Binary::TERRAGRUNT, Some(version)) if Version::parse(&version).is_ok() => {
-                    install_terragrunt_version(version, dot_dir_path).await
+                (Action::INSTALL, Binary::TERRAGRUNT, Some(version))
+                    if Version::parse(&version).is_ok() =>
+                {
+                    install_terragrunt_version(version, dot_dir, os)
                 }
                 (Action::SELECT, binary, Some(version)) if Version::parse(&version).is_ok() => {
-                    select_binary_version(binary, version, dot_dir_path)
+                    select_binary_version(binary, version, dot_dir)
                 }
                 (Action::REMOVE, binary, Some(version)) if Version::parse(&version).is_ok() => {
-                    remove_binary_version(binary, version, dot_dir_path)
+                    remove_binary_version(binary, version, dot_dir)
                 }
                 _ => Err("Invalid arguments. Run 'terve --help' for usage".into()),
             }
@@ -65,48 +81,44 @@ async fn run() -> Result<String, Box<dyn Error>> {
     }
 }
 
-fn create_dot_dir(home_dir: PathBuf) -> Result<String, Box<dyn Error>> {
-    let dot_dir_path = format!("{}/.terve", home_dir.display());
-    create_dir_all(format!("{}/bin", &dot_dir_path))?;
-    create_dir_all(format!("{}/opt/terraform", &dot_dir_path))?;
-    create_dir_all(format!("{}/opt/terragrunt", &dot_dir_path))?;
-    Ok(dot_dir_path)
+struct DotDir {
+    bin: PathBuf,
+    opt: PathBuf,
 }
 
-fn list_installed_versions(binary: Binary, dot_dir_path: String) -> Result<String, Box<dyn Error>> {
-    let opt_dir_path = format!("{}/opt/{}", dot_dir_path, binary);
-    let mut installed_versions: Vec<Version> = read_dir(&opt_dir_path)?
-        .filter_map(|r| {
-            Some(
-                r.ok()?
-                    .path()
-                    .strip_prefix(&opt_dir_path)
-                    .ok()?
-                    .to_path_buf(),
-            )
+impl DotDir {
+    fn init(home_dir: PathBuf) -> Result<DotDir, Box<dyn Error>> {
+        let bin_dir = home_dir.join(".terve/bin");
+        let opt_dir = home_dir.join(".terve/opt");
+        create_dir_all(&bin_dir)?;
+        create_dir_all(opt_dir.join("terraform"))?;
+        create_dir_all(opt_dir.join("terragrunt"))?;
+        Ok(DotDir {
+            bin: bin_dir,
+            opt: opt_dir,
         })
+    }
+}
+
+fn list_installed_versions(binary: Binary, dot_dir: DotDir) -> StringOrError {
+    let opt_dir = dot_dir.opt.join(binary);
+    let mut installed_versions: Vec<Version> = read_dir(&opt_dir)?
+        .filter_map(|r| Some(r.ok()?.path().strip_prefix(&opt_dir).ok()?.to_path_buf()))
         .filter_map(|p| Version::parse(p.display().to_string().as_str()).ok())
         .collect();
     installed_versions.sort();
     installed_versions.reverse();
     let result = installed_versions
-        .into_iter()
+        .iter()
         .map(|v| v.to_string())
         .collect::<Vec<String>>()
         .join("\n");
     Ok(result)
 }
 
-async fn list_available_terraform_versions() -> Result<String, Box<dyn Error>> {
+fn list_available_terraform_versions() -> StringOrError {
     let http_client = new_http_client()?;
-    let releases_html = http_client
-        .get(TF_RELEASES_URL)
-        .header("Accept", "text/html")
-        .send()
-        .await?
-        .error_for_status()?
-        .text()
-        .await?;
+    let releases_html = http_get_text(&http_client, TF_RELEASES_URL, "text/html")?;
     let semver_regex = Regex::new(r"[0-9]+\.[0-9]+\.[0-9]+").unwrap();
     let mut versions: Vec<&str> = semver_regex
         .find_iter(&releases_html)
@@ -117,7 +129,7 @@ async fn list_available_terraform_versions() -> Result<String, Box<dyn Error>> {
     Ok(result)
 }
 
-async fn list_available_terragrunt_versions() -> Result<String, Box<dyn Error>> {
+fn list_available_terragrunt_versions() -> StringOrError {
     let http_client = new_http_client()?;
     let mut releases: Vec<GitHubRelease> = Vec::new();
     // Max out at 1000 most recent releases
@@ -127,11 +139,9 @@ async fn list_available_terragrunt_versions() -> Result<String, Box<dyn Error>> 
             .header("Accept", "application/vnd.github.v3+json")
             .query(&[("per_page", "100")])
             .query(&[("page", page_num.to_string().as_str())])
-            .send()
-            .await?
+            .send()?
             .error_for_status()?
-            .json()
-            .await?;
+            .json()?;
         let num_results = page.len();
         releases.append(&mut page);
         if num_results < 100 {
@@ -140,120 +150,76 @@ async fn list_available_terragrunt_versions() -> Result<String, Box<dyn Error>> 
     }
     let versions: Vec<&str> = releases
         .iter()
-        .map(|r| r.tag_name.trim_start_matches('v'))
+        .map(|r| r.tag_name.trim_start_matches("v"))
         .collect();
     let result = versions.join("\n");
     Ok(result)
 }
 
-async fn install_terraform_version(
-    version: String,
-    dot_dir_path: String,
-) -> Result<String, Box<dyn Error>> {
-    let zip_download_url = format!(
-        "{0}{1}/terraform_{1}_linux_amd64.zip",
-        TF_RELEASES_URL, version
+fn install_terraform_version(version: String, dot_dir: DotDir, os: &str) -> StringOrError {
+    let file_download_url = format!(
+        "{0}{1}/terraform_{1}_{2}_amd64.zip",
+        TF_RELEASES_URL, version, os
     );
-    let sha256sums_download_url = format!("{0}{1}/terraform_{1}_SHA256SUMS", TF_RELEASES_URL, version);
+    let shasums_download_url = format!("{0}{1}/terraform_{1}_SHA256SUMS", TF_RELEASES_URL, version);
     let http_client = new_http_client()?;
-    let zip_file_bytes = http_client
-        .get(zip_download_url)
-        .header("Accept", "application/octet-stream")
-        .send()
-        .await?
-        .error_for_status()?
-        .bytes()
-        .await?;
-    let sha256_sums = http_client
-        .get(sha256sums_download_url)
-        .header("Accept", "text/plain")
-        .send()
-        .await?
-        .error_for_status()?
-        .text()
-        .await?;
+    let tmp_zip_file = tempfile::tempfile()?;
+    http_get_bytes(&http_client, &file_download_url, &tmp_zip_file)?;
+    let shasums = http_get_text(&http_client, &shasums_download_url, "text/plain")?;
     let shasum_regex = Regex::new(r"([a-f0-9]{64}).+_linux_amd64.zip").unwrap();
+    // TODO: fix
     let expected_sha256_sum = shasum_regex
-        .captures(&sha256_sums)
+        .captures(&shasums)
         .unwrap()
         .get(1)
         .unwrap()
         .as_str();
-    let actual_sha256_sum = sha256sum(&mut Cursor::new(&zip_file_bytes))?;
-    if &actual_sha256_sum != expected_sha256_sum {
-        Err(format!("File checksum mismatch, expected '{}', got '{}'", expected_sha256_sum, actual_sha256_sum))?;
-    }
-    let mut tmp_zip_file = tempfile::tempfile()?;
-    copy(&mut Cursor::new(&zip_file_bytes), &mut tmp_zip_file)?;
+    check_sha256sum(&tmp_zip_file, &expected_sha256_sum)?;
     let mut zip_archive = ZipArchive::new(tmp_zip_file)?;
     let mut binary_in_zip = zip_archive.by_name("terraform")?;
-    let bin_path = format!("{}/opt/terraform/{}", dot_dir_path, version);
-    let mut bin_file = File::create(&bin_path)?;
-    copy(&mut binary_in_zip, &mut bin_file)?;
+    let opt_file_path = dot_dir.opt.join(Binary::TERRAFORM).join(&version);
+    let mut opt_file = File::create(&opt_file_path)?;
+    copy(&mut binary_in_zip, &mut opt_file)?;
     #[cfg(unix)]
     {
         use std::os::unix::fs::PermissionsExt;
-        set_permissions(&bin_path, Permissions::from_mode(0o755))?;
+        set_permissions(&opt_file_path, Permissions::from_mode(0o755))?;
     }
     Ok(format!("Installed terraform {}", version))
 }
 
-async fn install_terragrunt_version(
-    version: String,
-    dot_dir_path: String,
-) -> Result<String, Box<dyn Error>> {
-    let bin_download_url = format!(
-        "{0}/v{1}/terragrunt_linux_amd64",
-        TG_RELEASES_DOWNLOAD_URL, version
+fn install_terragrunt_version(version: String, dot_dir: DotDir, os: &str) -> StringOrError {
+    let file_download_url = format!(
+        "{}/v{}/terragrunt_{}_amd64",
+        TG_RELEASES_DOWNLOAD_URL, version, os
     );
-    let sha256sums_download_url = format!("{0}/v{1}/SHA256SUMS", TG_RELEASES_DOWNLOAD_URL, version);
+    let shasums_download_url = format!("{0}/v{1}/SHA256SUMS", TG_RELEASES_DOWNLOAD_URL, version);
     let http_client = new_http_client()?;
-    let bin_file_bytes = http_client
-        .get(bin_download_url)
-        .header("Accept", "application/octet-stream")
-        .send()
-        .await?
-        .error_for_status()?
-        .bytes()
-        .await?;
-    let sha256_sums = http_client
-        .get(sha256sums_download_url)
-        .header("Accept", "text/plain")
-        .send()
-        .await?
-        .error_for_status()?
-        .text()
-        .await?;
+    let opt_file_path = dot_dir.opt.join(Binary::TERRAGRUNT).join(&version);
+    let opt_file = File::create(&opt_file_path)?;
+    http_get_bytes(&http_client, &file_download_url, &opt_file)?;
+    let sha256_sums = http_get_text(&http_client, &shasums_download_url, "text/plain")?;
     let sha256sum_regex = Regex::new(r"([a-f0-9]{64})\s+terragrunt_linux_amd64")?;
+    // TODO: fix
     let expected_sha256_sum = sha256sum_regex
         .captures(&sha256_sums)
         .unwrap()
         .get(1)
         .unwrap()
         .as_str();
-    let actual_sha256_sum = sha256sum(&mut Cursor::new(&bin_file_bytes))?;
-    if &actual_sha256_sum != expected_sha256_sum {
-        Err(format!("Binary sha256 checksum mismatch, expected '{}', got '{}'", expected_sha256_sum, actual_sha256_sum))?;
-    }
-    let bin_path = format!("{}/opt/terragrunt/{}", dot_dir_path, version);
-    let mut bin_file = File::create(&bin_path)?;
-    copy(&mut Cursor::new(&bin_file_bytes), &mut bin_file)?;
+    check_sha256sum(&File::open(&opt_file_path)?, &expected_sha256_sum)?;
     #[cfg(unix)]
     {
         use std::os::unix::fs::PermissionsExt;
-        set_permissions(&bin_path, Permissions::from_mode(0o755))?;
+        set_permissions(&opt_file_path, Permissions::from_mode(0o755))?;
     }
     Ok(format!("Installed terragrunt {}", version))
 }
 
-fn select_binary_version(
-    binary: Binary,
-    version: String,
-    dot_dir_path: String,
-) -> Result<String, Box<dyn Error>> {
-    let symlink_path = format!("{}/bin/{}", dot_dir_path, binary);
-    let bin_path = format!("{}/opt/{}/{}", dot_dir_path, binary, version);
-    if !Path::new(&bin_path).exists() {
+fn select_binary_version(binary: Binary, version: String, dot_dir: DotDir) -> StringOrError {
+    let symlink_path = dot_dir.bin.join(&binary);
+    let opt_file_path = dot_dir.opt.join(&binary).join(&version);
+    if !Path::new(&opt_file_path).exists() {
         Err(format!(
             "{0} version {1} is not installed. Run 'terve install {0} {1}'",
             binary, version
@@ -264,20 +230,16 @@ fn select_binary_version(
     }
     #[cfg(unix)]
     {
-        std::os::unix::fs::symlink(&bin_path, &symlink_path)?;
+        std::os::unix::fs::symlink(&opt_file_path, &symlink_path)?;
     }
     Ok(format!("Using {} {}", binary, version))
 }
 
-fn remove_binary_version(
-    binary: Binary,
-    version: String,
-    dot_dir_path: String,
-) -> Result<String, Box<dyn Error>> {
-    let symlink_path = format!("{}/bin/{}", dot_dir_path, binary);
-    let bin_path = format!("{}/opt/{}/{}", dot_dir_path, binary, version);
-    if Path::new(&bin_path).exists() {
-        remove_file(&bin_path)?;
+fn remove_binary_version(binary: Binary, version: String, dot_dir: DotDir) -> StringOrError {
+    let symlink_path = dot_dir.bin.join(&binary);
+    let opt_file_path = dot_dir.opt.join(&binary).join(&version);
+    if Path::new(&opt_file_path).exists() {
+        remove_file(&opt_file_path)?;
         let symlink_is_broken =
             read_link(&symlink_path).is_ok() && !Path::new(&symlink_path).exists();
         if symlink_is_broken {
@@ -336,7 +298,7 @@ impl FromStr for Binary {
             "tf" | "terraform" => Ok(Binary::TERRAFORM),
             "tg" | "terragrunt" => Ok(Binary::TERRAGRUNT),
             _ => Err(format!(
-                "binary must be one of: tf, tg, terraform, terragrunt"
+                "binary must be one of: tf, terraform, tg, terragrunt"
             )),
         }
     }
@@ -348,6 +310,16 @@ impl Display for Binary {
             Binary::TERRAFORM => write!(f, "terraform"),
             Binary::TERRAGRUNT => write!(f, "terragrunt"),
         }
+    }
+}
+
+impl AsRef<Path> for Binary {
+    fn as_ref(&self) -> &Path {
+        let path = match *self {
+            Binary::TERRAFORM => "terraform",
+            Binary::TERRAGRUNT => "terragrunt",
+        };
+        Path::new(path)
     }
 }
 
@@ -374,9 +346,40 @@ fn new_http_client() -> Result<Client, Box<dyn Error>> {
     Ok(client)
 }
 
-fn sha256sum<R>(mut reader: &mut R) -> Result<String, Box<dyn Error>> where R: Read {
+fn http_get_bytes(
+    http_client: &Client,
+    url: &str,
+    mut dest_file: &File,
+) -> Result<u64, Box<dyn Error>> {
+    let num_bytes = http_client
+        .get(url)
+        .header("Accept", "application/octet-stream")
+        .send()?
+        .copy_to(&mut dest_file)?;
+    Ok(num_bytes)
+}
+
+fn http_get_text(http_client: &Client, url: &str, accept: &str) -> StringOrError {
+    let text = http_client
+        .get(url)
+        .header("Accept", accept)
+        .send()?
+        .error_for_status()?
+        .text()?;
+    Ok(text)
+}
+
+fn check_sha256sum(mut file: &File, expected_sha256_sum: &str) -> Result<(), Box<dyn Error>> {
+    file.seek(SeekFrom::Start(0))?;
     let mut sha256 = Sha256::new();
-    copy(&mut reader, &mut sha256)?;
+    copy(&mut file, &mut sha256)?;
     let result = sha256.finalize();
-    Ok(hex::encode(result))
+    let actual_sha256_sum = hex::encode(result);
+    if &actual_sha256_sum != expected_sha256_sum {
+        Err(format!(
+            "File checksum mismatch: expected '{}', got '{}'",
+            expected_sha256_sum, actual_sha256_sum
+        ))?;
+    }
+    Ok(())
 }
